c5bcea1119e11577b8e3636ff06cc523d771b29b
loganhartford <logan.hartford@outlook.com>
2023-04-25 15:40:51 -0500

    am able to get list of nearby fastfood places now

--- api.py
+++ api.py
@@ -3,73 +3,54 @@
 import geocoder
 import requests
 
-user_loc = geocoder.ip('me')
+def search_for_nearby_establishment(lat, lng, radius, establishment, api_key):
+    places_base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    params = {
+        'location': f'{lat},{lng}',
+        'radius': radius,
+        'keyword': establishment,
+        'type': 'restaurant',
+        'key': api_key
+    }
+    response = requests.get(places_base_url, params=params)
+    data = response.json()
+    return data['results']
+
+def text_serach(query, radius, api_key):
+    places_base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    params = {
+        'query': query,
+        'location': f'{lat},{lng}',
+        'radius': radius,
+        'type': 'restaurant',
+        'key': api_key
+    }
+    response = requests.get(places_base_url, params=params)
+    data = response.json()
+    return data['results']
+    
 
 # Google Places API
-places_api_key = "AIzaSyBrIR_uTrPSrJ0QcmkWHo1tiT42X1CPudU"
-places_base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-
+user_loc = geocoder.ip('me')
 lat, lng = user_loc.latlng
-# Set up parameters for API request
-lat_lng = f"{lat},{lng}"  # Example latitude and longitude
-search_radius = "50000"  # Search radius in meters
-place_type = "restaurant"  # Type of place to search for
-# keyword = "(mcdonald) OR (wendy)"  # Keyword to search for
+places_api_key = "AIzaSyBrIR_uTrPSrJ0QcmkWHo1tiT42X1CPudU"
 
-# Construct URL for API request
-# url = f"{places_base_url}?location={lat_lng}&radius={search_radius}&type={place_type}&keyword={keyword}&key={places_api_key}"
-# url = f"{places_base_url}?location={lat_lng}&radius={search_radius}&key={places_api_key}"
+fast_food = ["MCDONALD'S", "SUBWAY", "TACO BELL", "CHICK-FIL-A", "WENDY'S", "BURGER KING", "DOMINO'S", "PIZZA HUT", "CHIPOTLE", "SONIC DRIVE-IN"]
 
-# # Send API request and get response
-# response = requests.get(url)
-# data = response.json()
-
-def get_nearby_places(lat, lng, radius, type_, api_key):
-    endpoint_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-    places = []
-    next_page_token = None
+# results = []
+# for establishment in fast_food:
+#     results.extend(search_for_nearby_establishment(lat, lng, 5000, establishment, places_api_key))
     
-    while True:
-        params = {
-            'location': f'{lat},{lng}',
-            'radius': radius,
-            'type': type_,
-            'key': api_key
-        }
-        if next_page_token:
-            params['pagetoken'] = next_page_token
-        response = requests.get(endpoint_url, params=params)
-        data = response.json()
-        places.extend(data['results'])
-        next_page_token = data.get('next_page_token')
-        if not next_page_token:
-            break
-    return places
-
-from bs4 import BeautifulSoup
-
-def get_fast_food_nearby(lat, lng):
-    url = f'https://www.google.com/maps/search/fast+food/@{lat},{lng},14z/data=!3m1!4b1'
-    print(url)
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # print(soup)
-    results = soup.find_all('div')
-    print(results)
-    return results
-
-places = get_fast_food_nearby(lat, lng)
-print(places)
-results = get_nearby_places(lat, lng, search_radius, place_type, places_api_key)
+results = text_serach('fast food', 5000, places_api_key)
 
 # Extract relevant information from API response
-# results = data["results"]
 print(len(results))
-locations = [(result["name"], result["vicinity"], result["geometry"]["location"]) for result in results]
+print(results[0])
+locations = [(result["name"], result["formatted_address"], result["geometry"]["location"]) for result in results]
 restaurant_names = {result["name"] for result in results}
+# print(locations)
 print(restaurant_names, len(restaurant_names))
 
-fast_food = ["MCDONALD'S", "SUBWAY", "TACO BELL", "CHICK-FIL-A", "WENDY'S", "BURGER KING", "DOMINO'S", "PANERA BREAD", "PIZZA HUT", "CHIPOTLE", "SONIC DRIVE-IN"]
 
 """
  What we want to do next
